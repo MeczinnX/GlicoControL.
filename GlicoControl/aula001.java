b7614b86bceb185bc4c181905ae6e1bd74919198
Wandrey Souza <130680594+MeczinnX@users.noreply.github.com>
2023-05-26 00:15:08 -0300

    Add files via upload

---
+++ GlicoControl/aula001.java
@@ -0,0 +1,12 @@
+class Programa {  
+  public static void main(String[] args) 
+  {
+    int i = 1;
+    System.out.println(i++);
+      int j = 1;
+    System.out.println(++j);
+
+    System.out.println(i + " | " + j);
+  }
+
+}
